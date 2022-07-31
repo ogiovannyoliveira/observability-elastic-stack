b076134e4ae06e3cd18c2cb9f89e9bfb10a691f3
Giovanny Lucas <giolucas@uolinc.com>
2022-07-31 16:06:07 -0300

    feat: :sparkles: dockerized golang application using multi-stage building

--- nodejs-server/main.js
+++ nodejs-server/main.js
@@ -3,7 +3,7 @@ import { initializeCron } from './cron.js';
 
 const server = createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end('Hello World\n');
+  response.end('Hello World!\n');
 });
 
 server.listen(3333, () => {
